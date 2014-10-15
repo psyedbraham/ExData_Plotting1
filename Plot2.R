0c207b4fb0d9f92a85f97fc6426dd32362a51543
psyedbraham <psyedbraham@users.noreply.github.com>
2014-10-14 21:54:58 -0500

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,3 +1,11 @@
+# read the data form the .txt file
+data <- read.table("~/household_power_consumption.txt",header = T, sep = ";",as.is=T,na.strings = "?")
+
+# convert and assing the formating for data field
+data$Date <- as.Date(data$Date,"%d/%m/%Y")
+
+# filter the date value based on the given dates
+data <- data[data$Date == '2007-02-01' | data$Date == '2007-02-02',]
 
 ## Plot 2
 plot(data$Global_active_power~data$Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
