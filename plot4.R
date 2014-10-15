6a1c330a79ac5bbdcd750580de7728a338b859e4
psyedbraham <psyedbraham@users.noreply.github.com>
2014-10-14 22:00:17 -0500

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,20 @@
+# read the data form the .txt file
+data <- read.table("~/household_power_consumption.txt",header = T, sep = ";",as.is=T,na.strings = "?")
+
+# convert and assing the formating for data field
+data$Date <- as.Date(data$Date,"%d/%m/%Y")
+
+# filter the date value based on the given dates
+data <- data[data$Date == '2007-02-01' | data$Date == '2007-02-02',]
+
+
+## Plot 4
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(data, {plot(Global_active_power~Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")plot(Voltage~Datetime, type="l",  ylab="Voltage (volt)", xlab="")plot(Sub_metering_1~Datetime, type="l",  ylab="Global Active Power (kilowatts)", xlab="")
+lines(Sub_metering_2~Datetime,col='Red')lines(Sub_metering_3~Datetime,col='Blue')legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))plot(Global_reactive_power~Datetime, type="l", 
+ylab="Global Rective Power (kilowatts)",xlab="")})
+
+## Saving to file
+dev.copy(png, file="plot4.png", height=480, width=480)
+dev.off()
